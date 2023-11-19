5b422ff44149d44ba9c1450e0a6d99b409839e58
jalalonagh <jalal.onagh@gmail.com>
2023-11-19 09:03:18 +0330

    start lesson 1

---
+++ BazargahNewTemplate/BazargahNewTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BazargahNewTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
